2b6da9e63e95886b833862757f9912efdbc3892e
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2021-12-03 15:59:16 +0530

    Initial commit

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import add
+
+def testAdd():
+    assert add(5,5) == 10 
+    print("Add function successfully executed!")
+
+if __name__ == "__main__":
+    testAdd()
---
+++ main.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a + b
+
+def sayHello():
+    print ("Hi, this is my first circle ci project")
+
+if __name__ == "__main__":
+    sayHello()
